e8046366dad8a656686658a0e847c073151793d8
AceCoder <107264922+AceCoder3@users.noreply.github.com>
2023-03-18 10:55:55 -0400

    Add files via upload

---
+++ All.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to streamlit ! 👋")
+
+st.sidebar.success("Select a method above.")
+
+st.markdown(
+    """
+This what you are seeing it the best thing that is closest to ChatGPT
+"""
+)
+
